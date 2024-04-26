95158f33c48cbae7597b17e696f4c426ec983878
brettleyorbrett <bretteverett00@gmail.com>
2024-04-26 16:39:17 +0000

    title stuff

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to Brett's application :]</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcome</h1>"));
         return done();
       });
 
